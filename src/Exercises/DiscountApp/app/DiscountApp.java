7fa13d98a580df7d6bc97adeb2ad2cf201e4fd14
n2God <kardasz.lukasz@gmail.com>
2019-06-08 11:09:16 +0200

    Discount app with java beans and second methods to print info

--- src/Exercises/DiscountApp/app/DiscountApp.java
+++ src/Exercises/DiscountApp/app/DiscountApp.java
@@ -2,6 +2,7 @@ package Exercises.DiscountApp.app;
 
 import Exercises.DiscountApp.client.Client;
 import Exercises.DiscountApp.service.DiscountService;
+import Exercises.DiscountApp.service.PrintService;
 
 /**
  * @author n2god on 08/06/2019
@@ -11,22 +12,27 @@ public class DiscountApp {
 
     public static void main(String[] args) {
 
-        Client client1 = new Client("Władek", "Kowalski", false);
+        Client client1 = new Client(null, "Kowalski", false);
         double price1 = 1100;
 
-        Client client2 = new Client("Remigiusz", "Kląb", true);
+        Client client2 = new Client(null, null, true);
         double price2 = 2000;
+
+        Client client3 = new Client("Jan", null, false);
+        double price3 = 3000;
 
         DiscountService discountService = new DiscountService();
         double discountPrice1 = discountService.calculateDiscountPrice(client1, price1);
         double discountPrice2 = discountService.calculateDiscountPrice(client2, price2);
+        double discountPrice3 = discountService.calculateDiscountPrice(client3, price3);
 
-        System.out.println("Witaj " + client1.getFirstName() + " " + client1.getLastName());
-        System.out.println("Kwota przed rabatem: " + price1);
-        System.out.println("Kwota po rabacie: " + discountPrice1);
+        PrintService printService = new PrintService();
+        printService.printInfo(client1, price1, discountPrice1);
+        printService.printInfo(client2, price2, discountPrice2);
+        printService.printInfo(client3, price3, discountPrice3);
 
-        System.out.println("Witaj " + client2.getFirstName() + " " + client2.getLastName());
-        System.out.println("Kwota przed rabatem: " + price2);
-        System.out.println("Kwota po rabacie: " + discountPrice2);
+
+
+
     }
 }
