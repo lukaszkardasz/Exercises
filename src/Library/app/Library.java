4500de02e22024939bc94e30171cc7e703b7b37b
n2God <kardasz.lukasz@gmail.com>
2019-06-09 09:37:41 +0200

    library - add arrays

--- src/Library/app/Library.java
+++ src/Library/app/Library.java
@@ -5,9 +5,12 @@ import Library.model.Book;
 public class Library {
 
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.5";
+        final String appName = "Biblioteka v0.7";
 
-        Book book1 = new Book("W pustyni i w puszczy",
+        Book[] books = new Book[1000];
+
+
+        books[0] = new Book("W pustyni i w puszczy",
                 "Henryk Sienkiewicz",
                 2010,
                 296,
@@ -15,14 +18,14 @@ public class Library {
                 "9788373271890"
                 );
 
-        Book book2 = new Book("Java. Efektywne programowanie. Wydanie II",
+        books[1] = new Book("Java. Efektywne programowanie. Wydanie II",
                 "Joshua Bloch",
                 2009,
                 352,
                 "Helion",
                 "9788324620845");
 
-        Book book3 = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide",
+        books[2] = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide",
                 "Bert Bates, Katherine Sierra",
                 2008,
                 851,
@@ -30,10 +33,9 @@ public class Library {
 
         System.out.println(appName);
         System.out.println("Książki dostępne w bibliotece:");
-        book1.printInfo();
-        book2.printInfo();
-        book3.printInfo();
-
-
+        books[0].printInfo();
+        books[1].printInfo();
+        books[2].printInfo();
+        System.out.println("System może przechowywać do " + books.length + " książek");
     }
 }
