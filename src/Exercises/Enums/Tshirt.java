25b578d8031f31a3c90c18184bf91bea1a269cc2
[n2god] <[n2god@o2.pl]>
2019-06-16 16:37:42 +0200

    enums exercises

---
+++ src/Exercises/Enums/Tshirt.java
@@ -0,0 +1,15 @@
+package Exercises.Enums;
+
+public class Tshirt {
+    private Size size;
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+
+}
