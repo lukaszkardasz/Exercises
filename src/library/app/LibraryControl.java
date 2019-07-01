ecdb68f7f0e9882c70870377f0253ad3dbc7e51f
n2God <kardasz.lukasz@gmail.com>
2019-07-01 22:36:56 +0200

    move enum to inner class

--- src/library/app/LibraryControl.java
+++ src/library/app/LibraryControl.java
@@ -109,4 +109,34 @@ public class LibraryControl {
         // zamykamy strumień wejścia
         dataReader.close();
     }
+
+    private enum Option{
+        EXIT(0, "Wyjscie z programu"),
+        ADD_BOOK(1, "Dodaj książkę"),
+        ADD_MAGAZINE(2, "Dodaj magazyn"),
+        PRINT_BOOKS(3, "Wypisz książki"),
+        PRINT_MAGAZINES(4, "Wypisz magazyny");
+
+        private int value;
+        private String description;
+
+
+        Option(int value, String description) {
+            this.value = value;
+            this.description = description;
+        }
+
+        @Override
+        public String toString() {
+            return value + " - " + description;
+        }
+
+        static Option createEnumFromInt(int option) throws NoSuchOptionException {
+            try {
+                return Option.values()[option];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new NoSuchOptionException("Brak opcji o id: " + option);
+            }
+        }
+    }
 }
