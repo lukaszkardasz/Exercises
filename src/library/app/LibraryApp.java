d481b304880a1bbf7496d5ac6adf5cd93b321983
n2God <kardasz.lukasz@gmail.com>
2019-07-15 21:11:41 +0200

    change version app to v2.1

--- src/library/app/LibraryApp.java
+++ src/library/app/LibraryApp.java
@@ -4,7 +4,7 @@ package library.app;
 
 
 public class LibraryApp {
-    private static final String APP_NAME = "Biblioteka v2.0";
+    private static final String APP_NAME = "Biblioteka v2.1";
 
     public static void main(String[] args) {
         System.out.println(APP_NAME);
