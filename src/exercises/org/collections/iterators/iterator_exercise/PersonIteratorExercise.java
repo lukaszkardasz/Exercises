acd307b26e6fdffecb1e94505dc01f1c38a9c9a0
n2God <kardasz.lukasz@gmail.com>
2019-07-17 22:14:29 +0200

    Iterator map exercise

--- src/exercises/org/collections/iterators/iterator_exercise/PersonIteratorExercise.java
+++ src/exercises/org/collections/iterators/iterator_exercise/PersonIteratorExercise.java
@@ -1,11 +1,26 @@
 package exercises.org.collections.iterators.iterator_exercise;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * @author n2god on 17/07/2019
  * @project Exercises
  */
 public class PersonIteratorExercise {
     public static void main(String[] args) {
+
+        Map<String, Person> namesMap = new TreeMap<>();
+
+        namesMap.put("z", new Person("Jan", "Nowak", 33));
+        namesMap.put("y", new Person("Paweł", "Kowalski", 53));
+        namesMap.put("f", new Person("Wacek", "Pracz", 13));
+
+        Iterator<Person> namesMapIterator = namesMap.values().iterator();
+        while (namesMapIterator.hasNext()){
+            System.out.println(namesMapIterator.next().toString());
+        }
 
     }
 }
