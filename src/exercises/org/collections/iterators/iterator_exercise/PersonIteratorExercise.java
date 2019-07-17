bdb8e8bccc2a4a8310e56be87e1dcc059cd7fceb
n2God <kardasz.lukasz@gmail.com>
2019-07-17 21:52:26 +0200

    refactor and new class Person

---
+++ src/exercises/org/collections/iterators/iterator_exercise/PersonIteratorExercise.java
@@ -0,0 +1,11 @@
+package exercises.org.collections.iterators.iterator_exercise;
+
+/**
+ * @author n2god on 17/07/2019
+ * @project Exercises
+ */
+public class PersonIteratorExercise {
+    public static void main(String[] args) {
+
+    }
+}
