a7d33594b5bebb24f702a6db992a35dc824b9eb5
n2God <kardasz.lukasz@gmail.com>
2019-07-18 22:23:37 +0200

    checkresult (print and equals numbers from two arrays - randomized and from user)

--- src/exercises/org/collections/exercise_lotto/Lotto.java
+++ src/exercises/org/collections/exercise_lotto/Lotto.java
@@ -1,8 +1,6 @@
 package exercises.org.collections.exercise_lotto;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 /**
  * @author n2god on 18/07/2019
@@ -14,10 +12,39 @@ public class Lotto {
     public static void main(String[] args) {
 
         generate(numbers);
-        System.out.println(numbers);
+        List<Integer> randomNumbers = numbers.subList(0, 6);
+        randomize(randomNumbers);
+        checkResult(numbers);
+
+        enterData();
+
+
+        System.out.println(randomNumbers);
 
 
 
+
+    }
+
+    private static void checkResult(List<Integer> numbers){
+
+    }
+
+    private static List<Integer> enterData() {
+        System.out.println("Podaj 6 cyfr: ");
+        List<Integer> numbersListFromUser = new ArrayList<>();
+        int count = 0;
+        while (count <= 6){
+            try(Scanner scanner = new Scanner(System.in)){
+                int fromUser = Integer.parseInt(scanner.nextLine());
+                numbersListFromUser.add(fromUser);
+            } catch (NumberFormatException e){
+                System.err.println("Niewłaściwe dane wejściowe, spróbuj jeszcze raz!");
+                continue;
+            }
+            count++;
+        }
+        return numbersListFromUser;
     }
 
     private static void generate(List<Integer> numbers) {
@@ -29,5 +56,9 @@ public class Lotto {
             int result = random.nextInt(high - low) + low;
             numbers.add(result);
         }
+    }
+
+    private static void randomize(List<Integer> randomNumbers) {
+        Collections.shuffle(randomNumbers);
     }
 }
