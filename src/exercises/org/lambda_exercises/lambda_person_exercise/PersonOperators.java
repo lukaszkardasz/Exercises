0155bee0dc6cb29c88289a6163a6293b151add43
n2God <kardasz.lukasz@gmail.com>
2019-07-19 21:16:36 +0200

    exercises with functional interfaces from util.functions - supplier, consumer, predicate and Function

--- src/exercises/org/lambda_exercises/lambda_person_exercise/PersonOperators.java
+++ src/exercises/org/lambda_exercises/lambda_person_exercise/PersonOperators.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class PersonOperators {
     public static void main(String[] args) {
@@ -15,22 +16,42 @@ public class PersonOperators {
         persons.add(new Person("Agnieszka", "Zagumna", 18));
         persons.add(new Person("Basia", "Cyniczna", 28));
 
+
+        System.out.println(">>>> wyswietlenie listy");
         consumeList(persons, p -> System.out.println(p));
-        System.out.println(">>>");
+
+        System.out.println();
+        System.out.println(">>>> operacja na wybranym kazdym elemencie listy");
         applyToList(persons, p -> {
-
-        }
-
-
+            p.setAge(p.getAge() + 1);
+            return p;
+        });
+        consumeList(persons, p -> System.out.println(p));
+        System.out.println();
+        System.out.println(">>>> operacja filtrowania na wybranym kazdym elemencie listy");
+        filterByPredicate(persons, p -> p.getAge() > 18);
 
     }
-    private static <T> void applyToList(List<T> list, Function<T, T> pre){
+
+    private static <T> void consumeList(List<T> list, Consumer<T> consumer) {
+        for (T t : list) {
+            consumer.accept(t);
+        }
+    }
+
+    private static <T> void applyToList(List<T> list, Function<T, T> function) {
         for (int i = 0; i < list.size(); i++) {
-            list.set(i, pre.apply(list.get(i)));
+            list.set(i, function.apply(list.get(i)));
         }
     }
 
-    private staticK
+    private static <T> void filterByPredicate(List<T> persons, Predicate<T> pre) {
+        for (T t : persons) {
+            if (pre.test(t)){
+                System.out.println(t);
+            }
+        }
+    }
 
 
 }
