d6b4a0c973678b3174826cf94907d5b3714ae601
[n2god] <[n2god@o2.pl]>
2019-07-22 22:40:30 +0200

    updated project to ref from java8

--- src/exercises/org/reference/reference_exercise/ReferenceExercise.java
+++ src/exercises/org/reference/reference_exercise/ReferenceExercise.java
@@ -1,24 +1,16 @@
 package exercises.org.reference.reference_exercise;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class ReferenceExercise {
 
     public static void main(String[] args) {
-        List<String> names = new ArrayList<>();
-        names.add("Kowalski");
-        names.add("Abacki");
-        names.add("Wójnicki");
-        names.add("Bednarek");
-        names.add("Krzywousty");
+        List<String> names = Arrays.asList("Karol", "Wojtek", "Alicja", "Bartek", "Romek");
 
-        Collections.sort(names, ReferenceExercise::sortAscending);
+        names.sort(String::compareToIgnoreCase);
         names.forEach(System.out::println);
-    }
-
-    private static int sortAscending(String s1, String s2){
-        return s1.compareToIgnoreCase(s2);
     }
 }
