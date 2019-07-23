f193861d9e8fd29183fea03ae8641d41cdb00329
[n2god] <[n2god@o2.pl]>
2019-07-23 22:31:57 +0200

    stream exercisees

---
+++ src/exercises/org/stream/StreamExercise.java
@@ -0,0 +1,28 @@
+package exercises.org.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamExercise {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        //Stream<Integer> stream = numbers.stream();
+
+        //filtrujemy liczby podzielne przez 2;
+        //stream.filter(x -> x % 2 == 0);
+
+        //i np wieksze od 5
+        //stream.filter(x -> x > 5);
+
+        //stream.filter(x -> x % 2 == 0 && x > 5);
+
+        numbers.stream()
+                .filter(x -> x % 2 == 0 && x > 5)
+                .collect(Collectors.toCollection(ArrayList::new))
+                .forEach(System.out::println);
+    }
+}
