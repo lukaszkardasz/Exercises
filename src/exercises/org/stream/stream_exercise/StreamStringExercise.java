e2ba6eeca397a3f893c318e20fbde3a82b1b49b0
[n2god] <[n2god@o2.pl]>
2019-07-25 23:02:22 +0200

    stream exercise  - fixed

--- src/exercises/org/stream/stream_exercise/StreamStringExercise.java
+++ src/exercises/org/stream/stream_exercise/StreamStringExercise.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,8 +15,25 @@ public class StreamStringExercise {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(PATH));) {
             List<String> words = bufferedReader.lines()
                     .map(line -> line.split(" "))
-                    .flatMap(Arrays::stream)
+                    .flatMap(Arrays::stream)//zmiana tablicy stringów (słowa w linii) na pojedyńcze słowa
+                    .map(s -> s.replaceAll(",","").replaceAll("\\.",""))
                     .collect(Collectors.toList());
+
+            long countWords = words.stream()
+                    .filter(s -> s.startsWith("s"))
+                    .peek(s -> System.out.print(s + " "))
+                    .count();
+
+            System.out.println();
+
+            long fiveLetters = words.stream()
+                    .filter(s -> s.length() == 5)
+                    .peek(s -> System.out.print(s + " "))
+                    .count();
+            System.out.println();
+            System.out.println("Liczba wyrazów na s: " + countWords);
+            System.out.println("Liczba 5literowych słów: " + fiveLetters);
+
         } catch (FileNotFoundException e) {
             System.err.println("Nie znaleziono pliku: " + PATH);
             e.printStackTrace();
