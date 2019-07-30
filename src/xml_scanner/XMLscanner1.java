697b7eb893463d58a3d1c5ef16987003332a5177
lukaszkardasz <kardasz.lukasz@gmail.com>
2019-07-30 19:32:39 +0200

    xml scanner - fixed but cant save in proper place

--- src/xml_scanner/XMLscanner1.java
+++ src/xml_scanner/XMLscanner1.java
@@ -4,14 +4,15 @@ import java.io.*;
 import java.util.Scanner;
 
 public class XMLscanner1 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Podaj ścieżkę do zeskanowania: ");
-        File currentDir = new File(sc.nextLine());
-        System.out.println("Podaj ścieżkę do umieszczenia pliku XML ze strukturą plików: ");
-        String xmlPath = sc.nextLine();
+    private static BufferedWriter writer;
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(xmlPath));) {
+    public static void main(String[] args) {
+        EnterData enterData = new EnterData().invoke();
+        File currentDir = enterData.getCurrentDir();
+        String xmlPath = enterData.getXmlPath();
+
+        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter(xmlPath));
             if(currentDir.exists() && currentDir.isDirectory()){
                 File files[] = currentDir.listFiles();
                 writer.write("<file list>");
@@ -20,14 +21,12 @@ public class XMLscanner1 {
 
                 writer.write("</file list>");
             }
-
-
         } catch (IOException e) {
             System.err.println("Nie udało się zapisać pliku: " + xmlPath);
         }
     }
 
-    private static void recursiveSaveToFile(File[] files, int index, int level) {
+    private static void recursiveSaveToFile(File[] files, int index, int level) throws IOException {
         //terminate condition
         if (index == files.length){
             return;
@@ -35,9 +34,43 @@ public class XMLscanner1 {
 
         //tabs for internal levels
         for (int i = 0; i < level; i++) {
-            writer.
+            writer.write("\t");
         }
+
+        //for files
+        if (files[index].isFile()){
+            writer.write(files[index].getName());
+            writer.newLine();
+        }
+        //for sub-directories
+        else if (files[index].isDirectory()){
+            writer.write("<" + files[index].getName() + ">");
+            //recursion for sub-directories
+            recursiveSaveToFile(files[index].listFiles(), 0, level + 1);
+            writer.write("</" + files[index].getName() + ">");
+        }
+        recursiveSaveToFile(files, ++index, level);
     }
 
+    public static class EnterData {
+        private File currentDir;
+        private String xmlPath;
 
+        public File getCurrentDir() {
+            return currentDir;
+        }
+
+        public String getXmlPath() {
+            return xmlPath;
+        }
+
+        public EnterData invoke() {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Podaj ścieżkę do zeskanowania: ");
+            currentDir = new File(sc.nextLine());
+            System.out.println("Podaj ścieżkę do umieszczenia pliku XML ze strukturą plików: ");
+            xmlPath = sc.nextLine() + "SCAN.xml";
+            return this;
+        }
+    }
 }
